b6c1a797dba936cc08fcbae834be440b1acdc701
vivace9842-rgb <vivace9842@gmail.com>
2026-04-23 22:30:19 -0300

    Fix syntax error in SUPABASE_URL declaration

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // CONFIGURAÇÃO ÚNICA - Preencha aqui e o sistema cuidará do resto
 const CONFIG = {
-    SUPABASE_URL: ''https://asblzyihnmnkmfitprcx.supabase.co',
+    SUPABASE_URL: 'https://asblzyihnmnkmfitprcx.supabase.co',
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzYmx6eWlobm1ua21maXRwcmN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MDcyNjgsImV4cCI6MjA5MjM4MzI2OH0.MuXquTfO9WQTwWs85Lh7is9wVFHV7x1si_M2JxAZiYc',
     GOOGLE_KEY: 'AIzaSyCIzabDvqzIqLFatrQx-MVOqXLh6urJAL0'
 };
